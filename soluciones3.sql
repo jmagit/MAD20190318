3ac6699fb7b7e66f1abfb7dc7d549212aeb34718
Javier Martin <35864946+jmagit@users.noreply.github.com>
2019-03-21 11:30:23 +0100

    Update soluciones3.sql

--- soluciones3.sql
+++ soluciones3.sql
@@ -34,4 +34,28 @@ FROM Production.Product
 GROUP BY [ProductSubcategoryID]
 ORDER BY [ProductSubcategoryID]
 
-
+						   
+SELECT  Name, [ListPrice], 
+	[ListPrice] - avg(ListPrice) over(partition by [ProductSubcategoryID]) [Delta precio],
+	[ListPrice] - max(ListPrice) over(partition by [ProductSubcategoryID]) [Delta máximo],
+	[ListPrice] - min(ListPrice) over(partition by [ProductSubcategoryID]) [Delta mínimo]
+FROM Production.Product
+SELECT  ProductSubcategoryID,Name, [ListPrice], 
+	rank() over(order by ListPrice desc) [Ranking de precios],
+	rank() over(partition by [ProductSubcategoryID] order by ListPrice desc) [Ranking de precios por categoria]
+FROM Production.Product
+SELECT  Name, [ListPrice],
+	case NTILE(3) over (order by [ListPrice] desc)
+	when 1 then 'Caro'
+	when 2 then 'Normal'
+	else 'Barato'
+	end Tipo
+	,
+	case 
+	when ListPrice = 0  then 'Regalao'
+	when ListPrice > 1000  then 'Caro'
+	when ListPrice between 100 and 1000 then 'Normal'
+	else 'Barato'
+	end 
+FROM Production.Product
+							
