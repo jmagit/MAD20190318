0b6ac22245a2130939452d44afab4e438cc99e7c
Javier Martin <35864946+jmagit@users.noreply.github.com>
2019-03-22 15:33:08 +0100

    Update ejemplos.sql

--- ejemplos.sql
+++ ejemplos.sql
@@ -66,3 +66,46 @@ FROM Production.Product p left join [Production].[UnitMeasure] u
 	left join [Production].[ProductModel] pm
 	on p.ProductModelID = pm.ProductModelID
 
+
+SELECT ISNULL(cast([ProductSubcategoryID] as varchar), '(Sin subcategoria)') [Sub categoria], 
+	COUNT(IIF(Class = 'L', Class, NULL)) Baja,
+	SUM(IIF(Class = 'M', 1, 0)) Media, SUM(IIF(Class = 'H', 1, 0)) Alta,
+	COUNT(class) Total
+FROM Production.Product
+GROUP BY [ProductSubcategoryID]
+ORDER BY [ProductSubcategoryID]
+go
+WITH cte(p, h, m, l) as (
+	SELECT * 
+	FROM (
+		SELECT [ProductSubcategoryID] p, class FROM Production.Product
+	) origen PIVOT (
+		count(class)
+		FOR class
+		IN ([H],[M], [L])
+	) cruzada
+)
+SELECT *
+FROM cte
+UNPIVOT (
+	Cuantos
+	FOR
+	Gama
+	IN ([H],[M],[L])
+) as T
+		     
+SELECT ProductSubcategoryID, 
+	(SELECT max(Name) 
+		from [Production].[ProductSubcategory] sc 
+		where ProductSubcategoryID is not null and
+			 sc.ProductSubcategoryID = ProductSubcategoryID) Categoria,
+	color
+FROM Production.Product
+where ListPrice > ANY ( select ListPrice FROM [dbo].[GamaAlta])
+--where color in (
+--	SELECT Color
+--	FROM Production.Product
+--	where ProductSubcategoryID = 1
+--)
+
+		     
